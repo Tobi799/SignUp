4421eaf4891fa850859f3015aff3fac3bccbe4be
Tobi <idowutobiloba1@gmail.com>
2023-06-17 17:12:37 +0100

    added

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ function signUp(event){
     event.preventDefault();
 
     const username = document.getElementById('signup-username').value;
+    const password = document.getElementById('signup-password').value;
+
+    if (username && password) {
+
+        localStorage.setItem('signup-username', username);
+        localStorage.setItem('signup-password', password);
+
+
+        document.getElementById('signup-username').value = "";
+        document.getElementById('signup-password').value = "";
+    }
+
 }
