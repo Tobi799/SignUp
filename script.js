47504e330513e7e855661810694726d9da3097e6
Tobi <idowutobiloba1@gmail.com>
2023-06-17 17:20:29 +0100

    added

--- script.js
+++ script.js
@@ -6,12 +6,34 @@ function signUp(event){
 
     if (username && password) {
 
-        localStorage.setItem('signup-username', username);
+        localStorage.setItem('signup-username', username);\
         localStorage.setItem('signup-password', password);
 
 
         document.getElementById('signup-username').value = "";
         document.getElementById('signup-password').value = "";
-    }
 
+        alert("Sign up successful");
+    } else {
+        alert("PLease enter a username and password.");
+    }
 }
+
+function login(event){
+    event.preventDefault();
+
+    const username = document.getElementById('login-username').value;
+    const password = document.getElementById('login-password').value;
+
+    const storedname = localStorage.getItem('username');
+
+    if (username === storedname && password) {
+        alert("Login successful");
+    } else {
+        alert("Invalid username or password");
+    }
+}
+
+
+document.getElementById("signup-form").addEventListener("submit", signUp)
+document.getElementById("login-form").addEventListener("submit", login)
